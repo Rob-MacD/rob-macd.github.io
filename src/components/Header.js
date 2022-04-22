b5800293924effa4ea4149d268847917f906a58b
Rob MacDonald <rob.m.macdonald@gmail.com>
2022-04-22 05:36:27 -0300

    Another attempt at not butchering this.

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Under Construction</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
