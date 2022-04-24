e90eb1d7de32a8fd06cb9257874f3c350e0ff4c1
Rob-MacD <rob.m.macdonald@gmail.com>
2022-04-24 06:26:38 -0300

    Added some CSS styles.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import "../styles/Header.css";
+
 class Header extends Component {
   state = {};
   render() {
     return (
       <div>
-        <h1>Under Construction!</h1>
-        <h2>Stay tuned...</h2>
+        <h1 className='h1'>Under Construction!</h1>
+        <h2 className='h2'>Stay tuned...</h2>
       </div>
     );
   }
